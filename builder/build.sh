f6f0897de33a50e34b832757e1bf9f0839bb46e8
Zac Shenker <zshenker@brightcove.com>
2015-03-26 13:23:14 -0700

    Adding extra debug logging. Now expecting the docker socket to be at /tmp/docker.sock, to fix issues with CircleCI builds.

--- builder/build.sh
+++ builder/build.sh
@@ -5,9 +5,21 @@ source /build_environment.sh
 # Compile statically linked version of package
 echo "Building $pkgName"
 CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' $pkgName
+echo "Finished building $pkgName"
 
-if [ -e "/var/run/docker.sock" ] && [ -e "./Dockerfile" ];
+if [ -e "/tmp/docker.sock" ]
 then
+  echo "Image build pre-check. Has Docker Socket"
+fi
+
+if [ -e "./Dockerfile" ]
+then
+  echo "Image build pre-check. Has Dockerfile"
+fi
+
+if [ -e "/tmp/docker.sock" ] && [ -e "./Dockerfile" ];
+then
+  echo "Starting to build docker image"
   # Grab the last segment from the package name
   name=${pkgName##*/}
 
@@ -15,5 +27,7 @@ then
   tagName=${tagName:-"$name":latest}
 
   # Build the image from the Dockerfile in the package directory
-  docker build -t $tagName .
+  docker -H unix:///tmp/docker.sock build -t $tagName .
+
+  echo "Finished building docker image"
 fi
